d866affc227669bf6d9656a8c98bda1db96e354b
ECarry <joklian@live.com>
2018-01-25 10:46:07 +0800

    添加注释

--- newSpider.py
+++ newSpider.py
@@ -12,7 +12,7 @@ def get_urls():
     http://bt0.com/film-download/1-0-0-0-0-1.html
     http://bt0.com/film-download/1-0-0-0-0-1018.html
     '''
-    page_urls = ['http://bt0.com/film-download/1-0-0-0-0-{cnt}.html'.format(cnt=cnt) for cnt in range(1, 2)]
+    page_urls = ['http://bt0.com/film-download/1-0-0-0-0-{cnt}.html'.format(cnt=cnt) for cnt in range(1, 2)] #在这更改要爬取的页面
     print("Please wait for second ...")
     movie_urls =[]
     for page_url in page_urls:                                      #在每个页面爬取所有电影的地址
